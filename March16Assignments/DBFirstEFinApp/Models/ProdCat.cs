c0e9b0594421d288158eb7012349de0e568dd132
Nehabarnwal24 <nehabarnwal9794@gmail.com>
2026-03-18 14:40:08 +0530

    assignment Added

---
+++ March16Assignments/DBFirstEFinApp/Models/ProdCat.cs
@@ -0,0 +1,9 @@
+﻿namespace DBFirstEFinApp.Models
+{
+    public class ProdCat
+    {
+        public string? prodname {  get; set; }
+        public string? catname {  get; set; }
+
+    }
+}
---
+++ March16Assignments/DBFirstEFinApp/Models/View1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBFirstEFinApp.Models;
+
+public partial class View1
+{
+    public string CompanyName { get; set; } = null!;
+
+    public string? City { get; set; }
+
+    public string? ContactName { get; set; }
+
+    public DateTime? OrderDate { get; set; }
+
+    public string CustomerId { get; set; } = null!;
+
+    public string? Expr1 { get; set; }
+
+    public int OrderId { get; set; }
+
+    public decimal? Freight { get; set; }
+}
