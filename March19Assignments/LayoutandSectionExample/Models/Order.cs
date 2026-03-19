a9cce2698d73d4d653838994366d180a78b19045
Nehabarnwal24 <nehabarnwal9794@gmail.com>
2026-03-19 15:54:00 +0530

    march19 Files Added

---
+++ March19Assignments/LayoutandSectionExample/Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace LayoutandSectionExample.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string? CustomerId { get; set; }
+        public DateTime? OrderDate { get; set; }
+    }
+}
