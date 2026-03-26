fdca66ed79f25ca42014b1c7595911d88fddb5cd
Nehabarnwal24 <nehabarnwal9794@gmail.com>
2026-03-26 15:42:51 +0530

    Files Added

---
+++ March25Assignments/WebApiInAspdemo/WebApiInAspdemo/Models/EmpContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiInAspdemo.Models;
+
+namespace WebApiInAsp.netcoreMvcDemo.Models
+{
+    public class EmpContext : DbContext
+    {
+        public EmpContext(DbContextOptions dbContextOptions) :
+            base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Employee> employees { set; get; }
+
+    }
+}
+
